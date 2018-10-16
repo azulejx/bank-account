b4de4adbdfe678c1f767cd4f1131b848a67920c3
Azul Rodriguez Da Silva and Quinn Miller <azul@>
2018-10-15 19:21:46 -0700

    separated our business logic and ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,49 @@
+//business logic start
+function Account(name, balance) {
+  this.name = name;
+  this.balance = balance;
+};
+Account.prototype.adjust = function(adjustment) {
+  this.balance = this.balance + adjustment;
+};
+var newAccount = {
+  name: "none",
+  balance: "none"
+};
+function adjustFunction(dep, draw) {
+  if (isNaN(dep) && isNaN(draw)) {
+    alert("Please input a number");
+    finalAdjustment = 0;
+  }
+  else if (isNaN(dep)) {
+    finalAdjustment = (-1 *draw);
+  }
+  else if (isNaN(draw)) {
+    finalAdjustment = dep;
+  }
+  else {
+    finalAdjustment = dep - draw;
+  }
+};
+//business logic end
+//ui logic start
 $(document).ready(function(){
-  function Account(name, balance) {
-    this.name = name;
-    this.balance = balance;
-  };
-  Account.prototype.adjust = function(adjustment) {
-    this.balance = this.balance + adjustment;
-  };
-  var newAccount = {
-    name: "none",
-    balance: "none"
-  };
-  function adjustFunction(dep, draw) {
-    if (isNaN(dep) && isNaN(draw)) {
-      alert("Please input a number");
-      finalAdjustment = 0;
-    }
-    else if (isNaN(dep)) {
-      finalAdjustment = (-1 *draw);
-    }
-    else if (isNaN(draw)) {
-      finalAdjustment = dep;
-    }
-    else {
-      finalAdjustment = dep - draw;
-    }
-  };
 
   $("button#makeaccount").click(function(){
     $("form#new-account").submit(function(event){
       event.preventDefault();
     });
-    var name = $("#new-name").val();
-    var initialDeposit = parseFloat($("#initial-deposit").val());
-    newAccount = new Account(name, initialDeposit);
-    $("#your-account").show();
-    $("#account-name").text(newAccount.name + "'s ");
-    $("#account-balance").text("$" + newAccount.balance);
+    if (newAccount.name === "none") {
+      alert("No information given");
+    }
+    else {
+      var name = $("#new-name").val();
+      var initialDeposit = parseFloat($("#initial-deposit").val());
+      newAccount = new Account(name, initialDeposit);
+      $("#your-account").show();
+      $("#account-name").text(newAccount.name + "'s ");
+      $("#account-balance").text("$" + newAccount.balance);
+    };
   });
   $("button#submit").click(function(){
     $("form#new-account").submit(function(event){
@@ -55,3 +63,4 @@ $(document).ready(function(){
     $("#account-balance").text("$" + newAccount.balance);
   });
 });
+//ui logic end
